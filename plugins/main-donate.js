1aeae10e71263ae2605bc8a3cf3f249c72f2b453
Red Alpha <120353204+RedAlpha137@users.noreply.github.com>
2022-12-11 20:31:34 -0800

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ≡ *DONATION*
 ONLY WANT YOUR LOVE
 SUPPORT ME ON INSTAGRAM`
-let img = 'https://raw.githubusercontent.com/RedAlpha/api/Alpha/alpha.jpg'
+let img = 'https://raw.githubusercontent.com/RedAlpha137/api/Alpha/alpha.jpg'
 conn.sendHydrated2(m.chat, don, '▢ Life Is Very Short Always Be Happy\n▢ Chintu-Bot\n▢ Subscribe  Youtube\nhttps://youtube.com/@user-gq5sx7cg4v\n', img,'https://www.instagram.com/mukarrammhmd', 'Instagram', 'https://t.me/redalpha1', 'TELEGRAM', [
     ], m)
 }
